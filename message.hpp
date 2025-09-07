9340d58404cea419e60d05fdbbc06dbaad62dea3
Your Name <manvendrasaini2005@gmail.com>
2025-09-08 03:08:52 +0530

    Remove unnecessary comment from message.hpp

--- message.hpp
+++ message.hpp
@@ -1,14 +1,10 @@
-//--> how to remotely connect to UTM(for mac) ubuntu server from vscode ,for windows wsl
 #include<iostream>
 #include<string>
 #include<string.h>
 //#include<boost/asio.hpp>
 
-
 #ifndef MESSAGE_HPP
 #define MESSAGE_HPP
-
-//using boost::asio::ip::tcp;
 
 class Message{
 
